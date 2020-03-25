acb9d460a30758aad527645c540c127d9722fa18
Benjamin Wichser <benwichser@gmail.com>
2020-03-25 22:24:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_w_bbq = []
-  holiday_hash.each do 
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      holiday_w_bbq << holiday.capitalize if supplies.include?("BBQ")
 end
 
 
